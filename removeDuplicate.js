82852eef2b228357a789848f00e021e0b68e2988
Yash-Ghatge <yashghatge012@gmail.com>
2025-09-11 15:42:22 +0530

    Update

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,4 +1,11 @@
 function remove(arr){
     let unique = []
-    
+    for (let num of arr) {
+        if (!unique.includes(num)) {
+            unique.push(num)
+        }
+    }
+    return unique
 }
+
+console.log(remove([1,2,3,3,4,5,5,6]))

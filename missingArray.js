77deb70076c84424dd5bb92585432e57b33e0b1e
Yash-Ghatge <yashghatge012@gmail.com>
2025-10-23 16:43:50 +0530

    Update

--- missingArray.js
+++ missingArray.js
@@ -1,7 +1,32 @@
+// function miss(arr,n){
+//     let total = (n*(n+1)) / 2
+//     let sum = arr.reduce((a,b)=>a+b,0)
+//     return total-sum
+// }
+
+// console.log(miss([1,2,3,4,6,7],7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function miss(arr,n){
-    let total = (n*(n+1)) / 2
+    let total = (n*(n+1))/2
     let sum = arr.reduce((a,b)=>a+b,0)
     return total-sum
 }
 
-console.log(miss([1,2,3,4,6,7],7))
+console.log(miss([1,2,3,5,6],6))

7a8a749a93d1e4b0422199d28acadd5b9409dcab
Yash-Ghatge <yashghatge012@gmail.com>
2025-11-01 20:44:23 +0530

    Update

--- twoSum.js
+++ twoSum.js
@@ -1,33 +1,3 @@
-// function twoSum(arr,target){
-//     let map ={}
-//     for (let i = 0; i < arr.length; i++) {
-//         let comp = target - arr[i];
-//         if (map[comp] !== undefined) {
-//             return [map[comp],i]
-//         }
-//         map[arr[i]] = i
-//     }
-//     return []
-// }
-
-// console.log(twoSum([2,3,6,8],10))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function twoSum(arr,target){
     let map = {}
     for (let i = 0; i < arr.length; i++) {

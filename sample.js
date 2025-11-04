732a88aef0564e2806833111a9385e41e097e177
Yash-Ghatge <yashghatge012@gmail.com>
2025-11-04 20:29:26 +0530

    Update

--- sample.js
+++ sample.js
@@ -53,3 +53,31 @@ const pattern = (n) => {
 }
 
 pattern(5)
+
+
+const isPalindrome = (str) => {
+    let left = 0 , right = str.length-1
+    while (left<right) {
+        if (str[left] !== str[right]) return false
+        right--
+        left++
+    }
+    return true
+}
+
+console.log(isPalindrome("poop"))
+
+const fibonacci = (n) => {
+    let term = []
+    if (n <= 0) return term
+    if (n >= 1) term.push(0)
+    if (n >= 2) term.push(1)
+    
+    for (let i = 2; i < n; i++) {
+        term.push(term[i-1]+term[i-2])
+    }
+    return term
+
+}
+
+console.log(fibonacci(4))

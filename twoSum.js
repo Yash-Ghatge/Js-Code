a40efcbdd6a2cec54a90311dce6469ffa3f469df
Yash-Ghatge <yashghatge012@gmail.com>
2025-11-04 19:24:18 +0530

    Practice

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,32 @@
-function twoSum(arr,target){
+// function twoSum(arr,target){
+//     let map = {}
+//     for (let i = 0; i < arr.length; i++) {
+//         let comp = target - arr[i]
+//         if (map[comp] !== undefined) {
+//             return [map[comp],i]
+//         }
+//         map[arr[i]] = i
+//     }
+//     return []
+// }
+
+// console.log(twoSum([1,2,4],3))
+
+
+
+
+
+const twosum = (array,target) => {
     let map = {}
-    for (let i = 0; i < arr.length; i++) {
-        let comp = target - arr[i]
+    for (let i = 0; i < array.length; i++) {
+        let comp = target - array[i]
         if (map[comp] !== undefined) {
             return [map[comp],i]
         }
-        map[arr[i]] = i
+        map[array[i]] = i
     }
     return []
 }
 
-console.log(twoSum([1,2,4],3))
+console.log(twosum([1,2,7,8,3],9))
+
